76224f47bf2237393dc2e580adf9ff93e7fe747f
guacajoely <guacajoely@gmail.com>
2023-04-25 14:46:17 -0400

    merging master

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -19,21 +19,65 @@ export const Events = () => {
         }, []
     )
 
+    const getMonthName = (monthNum) => {
+        const date = new Date()
+        date.setMonth(monthNum - 1)
+        return date.toLocaleString('en-US', { month: 'long' })
+    }
+
+    const createEventList = (eventsArray) => {
+
+        return eventsArray.map(singleEvent => {
+
+            return <section className="event" key={`event--${singleEvent.id}`}>
+                        <div className="event--name">
+                            <a href={`/event/edit/${singleEvent.id}`} title="Click to edit">{singleEvent.name}</a>
+                        </div>
+                        <div className="event--location">Location: {singleEvent.location} </div>
+                        <div className="event--date">Date: {singleEvent.date} </div>
+                    </section>
+        })
+    }
+
+    const eventsByMonth = () => {
+
+        const jsxArray = []
+        
+        for(let i = 1; i < 13; i++){
+
+            const monthlyEvents = []
+            let eventCount = 0
+            events.forEach(event => {
+
+                const [,eventMonth] = event.date.split("-")
+                if(parseInt(eventMonth) === i){
+                    monthlyEvents.push(event)
+                    eventCount++
+                }
+            })
+            console.log(monthlyEvents)
+
+            if(eventCount !== 0){
+                jsxArray.push(
+                <div className="event--list">
+                    <h3>{getMonthName(i)} ({eventCount})</h3>
+                    <div>{createEventList(monthlyEvents)}</div>
+                </div>
+                )
+            }
+        }
+
+        console.log(jsxArray)
+        return jsxArray
+    }
+    
+
+
+    
+
     return <article className="events">
 
-        {
-            events.map(event => {
-
-                return <section className="event" key={`event--${event.id}`}>
-                            <div className="event--name">
-                                <a href={`/event/edit/${event.id}`} title="Click to edit">{event.name}</a>
-                            </div>
-                            <div className="event--location">Location: {event.location} </div>
-                            <div className="event--date">Date: {event.date} </div>
-                        </section>
-            })
-
-        }
+        {eventsByMonth()}
 
         <button onClick={() => navigate("/event/create")}>Add Event</button>
 
