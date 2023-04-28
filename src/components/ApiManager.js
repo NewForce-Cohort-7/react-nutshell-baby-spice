2ee09f732974fbfc76ac2f46f41ef4977ac07922
Arnold Rispress <arnold.rispress@gmail.com>
2023-04-28 13:35:49 -0400

    added ability to delete posts

--- src/components/ApiManager.js
+++ src/components/ApiManager.js
@@ -135,9 +135,11 @@ export const getArticleById = (id) => {
     .then(r => r.json())
 }
 
-export const getArticleTags = () => {
-    return fetch(`http://localhost:8088/tags`)
-    .then(r => r.json())
+export const deleteArticle = (id) => {
+    return fetch(`http://localhost:8088/articles/${id}`, {
+      method: "DELETE",
+    })
+    
 }
 
 export const getFriendsByFriendId = (id) => {
